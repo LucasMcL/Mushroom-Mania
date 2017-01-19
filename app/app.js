b7a4b5dbc0ae7a6a26ccdda373ea1adda0266478
LucasMcL <LMcLaughlin09@gmail.com>
2017-01-19 13:47:07 -0600

    controller/factory and ng-repeat

--- app/app.js
+++ app/app.js
@@ -1,1 +1,45 @@
-var app = angular.module('MushroomMania', ['ngRoute'])
+////////////////////////////
+// Mushroom App
+////////////////////////////
+
+angular.module('MushroomMania', ['ngRoute'])
+	.config( ($routeProvider) => {
+		$routeProvider
+			.when('/', {
+				controller: 'mainCtrl',
+				templateUrl: '../partials/main-page.html'
+			})
+			.otherwise('/', {
+				controller: 'mainCtrl',
+				templateUrl: '../partials/main-page.html'
+			})
+	})
+	.controller('mainCtrl', function($scope, mushroomFactory) {
+		mushroomFactory
+			.getMushrooms()
+			.then((mushroomData) => $scope.mushrooms = mushroomData)
+	})
+	.factory('mushroomFactory', function($http) {
+		return {
+			getMushrooms: function() {
+				return $http({
+					method: "GET",
+					url: 'https://mushroom-mania-9035e.firebaseio.com/.json'
+				})
+				.then( (responseObj) => {
+					let data = responseObj.data
+					let key = Object.keys(data) // firebase rand key
+					return data[key].mushrooms
+				})
+			}
+		}
+	})
+
+
+
+
+
+
+
+
+
